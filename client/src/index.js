6eb1a2a69ef908c2be4704725de3773cb7ce7958
chiefmikey <wolfemikl@gmail.com>
2021-08-07 23:02:34 -0600

    Add zoom button

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,14 @@ import k from './init.js';
 import main from './scenes/main.js';
 import win from './scenes/win.js';
 
+document.getElementById('zoom').addEventListener('click', function () {
+  if (document.getElementById('app').style.height === '100%') {
+    document.getElementById('app').style.height = '190%';
+  } else {
+    document.getElementById('app').style.height = '100%';
+  }
+});
+
 k.scene('main', main);
 k.scene('win', win);
 k.start('main');
