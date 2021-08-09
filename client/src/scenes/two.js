02a9c90954b68f99ac97622257b9cdcf182b7154
chiefmikey <wolfemikl@gmail.com>
2021-08-09 16:49:05 -0600

    Separate scene two

---
+++ client/src/scenes/two.js
@@ -0,0 +1,40 @@
+import K from '../functions/init.js';
+import characters from '../content/characters.js';
+
+const two = (controls) => {
+  const level = [
+    '=======F=======',
+    '=             =',
+    '=             =',
+    '=     !       =',
+    '=             =',
+    '=             =',
+    '=             =',
+    '=             =',
+    '=             =',
+    '=             =',
+    '=             =',
+    '=             =',
+    '=             =',
+    '=      @      =',
+    '===============',
+  ];
+
+  K.addLevel(level, {
+    width: 11,
+    height: 11,
+    pos: K.vec2(20, 20),
+    '=': [K.sprite('steel'), K.solid()],
+    '@': [K.sprite('guy'), 'playerOne'],
+    '!': [
+      K.sprite(characters.cloudyman.sprite),
+      K.solid(),
+      characters.cloudyman.sprite,
+      { msg: characters.cloudyman.msg },
+    ],
+    F: [K.sprite('door'), K.solid(), 'finalDoor'],
+  });
+  controls();
+};
+
+export default two;
