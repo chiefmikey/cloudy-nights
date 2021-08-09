1584b5293f9bfb2f3f6c8a16f049c70f7677364a
chiefmikey <wolfemikl@gmail.com>
2021-08-09 16:48:40 -0600

    Import controls

--- client/src/index.js
+++ client/src/index.js
@@ -1,29 +1,11 @@
-import k from './init.js';
-import main from './scenes/main.js';
+import K from './functions/init.js';
+import one from './scenes/one.js';
+import two from './scenes/two.js';
 import win from './scenes/win.js';
+import controls from './functions/controls.js';
 
-let isZoomed = false;
+K.scene('one', () => one(controls));
+K.scene('two', () => two(controls));
+K.scene('win', win);
 
-document.getElementById('zoom').addEventListener('click', function () {
-  if (!isZoomed) {
-    document.getElementById('body').style.justifyContent = 'flex-start';
-    document.getElementById('app').style.justifyContent = 'flex-start';
-    document.getElementById('app').style.height = '190%';
-    document.getElementById('zoom').style.visibility = 'visible';
-    document.getElementById('title').style.transform = 'scale(2.1)';
-    document.getElementById('background').style.maxWidth = 'none';
-    isZoomed = true;
-  } else {
-    document.getElementById('body').style.justifyContent = 'center';
-    document.getElementById('app').style.justifyContent = 'center';
-    document.getElementById('app').style.height = '100%';
-    document.getElementById('zoom').style.visibility = '';
-    document.getElementById('title').style.transform = 'scale(1)';
-    document.getElementById('background').style.maxWidth = '100%';
-    isZoomed = false;
-  }
-});
-
-k.scene('main', main);
-k.scene('win', win);
-k.start('main');
+K.start('one');
