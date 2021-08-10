95d80a6e906806f3283f98622c7fa661fd279f51
chiefmikey <wolfemikl@gmail.com>
2021-08-09 22:26:04 -0600

    Update talking to array

--- client/src/content/talking.js
+++ client/src/content/talking.js
@@ -1,22 +1,30 @@
 import K from '../functions/init.js';
 
-let talking = null;
+let talking = [];
 
 export const ifTalking = () => {
-  if (talking) {
-    K.destroy(talking);
-    talking = null;
+  if (talking[0]) {
+    talking.forEach((t) => {
+      K.destroy(t);
+    });
+    talking = [];
   }
 };
 
 export const talk = (msg) => {
-  talking = K.add([
-    K.text(msg, 7, { width: 140 }),
-    K.pos(K.width() / 2, K.height() - 11),
-    K.origin('center'),
-  ]);
+  ifTalking();
+  talking.push(
+    K.add([
+      K.text(msg, 7, { width: 140 }),
+      K.pos(K.width() / 2, K.height() - 11),
+      K.origin('center'),
+    ]),
+  );
 };
 
 export const cloudyTalk = (msg) => {
-  talking = K.add([K.text(msg), K.pos(K.width() / 2, 11), K.origin('center')]);
+  ifTalking();
+  talking.push(
+    K.add([K.text(msg), K.pos(K.width() / 2, 11), K.origin('center')]),
+  );
 };
