04d95d40ea3a7f3eb9cc1035aacbef748dbd08a9
chiefmikey <wolfemikl@gmail.com>
2021-08-09 22:25:32 -0600

    Remove portrait lock js solution

--- client/src/index.js
+++ client/src/index.js
@@ -4,8 +4,6 @@ import two from './scenes/two.js';
 import win from './scenes/win.js';
 import controls from './functions/controls.js';
 
-const lockScreen = window.screen.lockOrientation('portrait');
-
 K.scene('one', () => one(controls));
 K.scene('two', () => two(controls));
 K.scene('win', win);
