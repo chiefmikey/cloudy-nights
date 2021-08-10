7938966d37146dd478ba971eb09488ad8a4b96ee
chiefmikey <wolfemikl@gmail.com>
2021-08-10 16:48:34 -0600

    Fix controls direction change

--- client/src/functions/controls.js
+++ client/src/functions/controls.js
@@ -1,7 +1,46 @@
 import K from './init.js';
-import playerOne from '../content/player.js';
 import { ifTalking } from '../content/talking.js';
+import playerOne from '../content/player.js';
 import './zoom.js';
+
+let getMoving = () => {};
+let isDown = false;
+let direction;
+let timer;
+
+const playerMove = (e) => {
+  direction = e.target.id;
+  if (
+    e.type !== 'mouseup' &&
+    e.type !== 'touchend' &&
+    e.type !== 'touchcancel' &&
+    (e.target.id === 'up' ||
+      e.target.id === 'topRight' ||
+      e.target.id === 'right' ||
+      e.target.id === 'bottomRight' ||
+      e.target.id === 'down' ||
+      e.target.id === 'bottomLeft' ||
+      e.target.id === 'left' ||
+      e.target.id === 'topLeft') &&
+    (e.buttons === 1 || e.buttons === 3)
+  ) {
+    clearInterval(timer);
+    isDown = true;
+    getMoving();
+  } else {
+    clearInterval(timer);
+    isDown = false;
+  }
+  e.preventDefault();
+};
+
+document.addEventListener('mousedown', playerMove);
+document.addEventListener('mouseup', playerMove);
+document.addEventListener('mouseover', playerMove);
+document.addEventListener('touchstart', playerMove);
+document.addEventListener('touchend', playerMove);
+document.addEventListener('touchmove', playerMove);
+document.addEventListener('touchcancel', playerMove);
 
 const controls = () => {
   const player = playerOne();
@@ -9,8 +48,8 @@ const controls = () => {
 
   const dirs = {
     left: K.vec2(-1, 0),
-    right: K.vec2(1, 0),
     up: K.vec2(0, -1),
+    right: K.vec2(1, 0),
     down: K.vec2(0, 1),
   };
 
@@ -22,36 +61,28 @@ const controls = () => {
     });
   }
 
-  let isDown = false;
-  let timer;
-
-  const playerMove = (e) => {
-    if (e.type !== 'mouseup' && e.type !== 'touchend') {
-      ifTalking();
-      isDown = true;
-      timer = setInterval(() => {
-        if (isDown) {
-          player.move(dirs[e.target.id].scale(SPEED));
+  getMoving = () => {
+    ifTalking();
+    timer = setInterval(() => {
+      if (isDown) {
+        if (direction === 'topRight') {
+          player.move(dirs.right.scale(SPEED));
+          player.move(dirs.up.scale(SPEED));
+        } else if (direction === 'bottomRight') {
+          player.move(dirs.right.scale(SPEED));
+          player.move(dirs.down.scale(SPEED));
+        } else if (direction === 'bottomLeft') {
+          player.move(dirs.left.scale(SPEED));
+          setTimeout(() => player.move(dirs.down.scale(SPEED)), 0);
+        } else if (direction === 'topLeft') {
+          player.move(dirs.left.scale(SPEED));
+          setTimeout(() => player.move(dirs.up.scale(SPEED)), 0);
+        } else {
+          player.move(dirs[direction].scale(SPEED));
         }
-      }, 15);
-    } else {
-      clearInterval(timer);
-      isDown = false;
-    }
-    e.preventDefault();
+      }
+    }, 15);
   };
-
-  document.getElementById('up').addEventListener('mousedown', playerMove);
-  document.getElementById('up').addEventListener('touchstart', playerMove);
-  document.getElementById('right').addEventListener('mousedown', playerMove);
-  document.getElementById('right').addEventListener('touchstart', playerMove);
-  document.getElementById('down').addEventListener('mousedown', playerMove);
-  document.getElementById('down').addEventListener('touchstart', playerMove);
-  document.getElementById('left').addEventListener('mousedown', playerMove);
-  document.getElementById('left').addEventListener('touchstart', playerMove);
-
-  document.addEventListener('mouseup', playerMove);
-  document.addEventListener('touchend', playerMove);
 };
 
 export default controls;
