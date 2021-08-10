63e1b40b9a41b0c118a5d8e5b9ca9943429d41a3
chiefmikey <wolfemikl@gmail.com>
2021-08-10 16:49:18 -0600

    Remove controls import

--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,9 @@ import K from './functions/init.js';
 import one from './scenes/one.js';
 import two from './scenes/two.js';
 import win from './scenes/win.js';
-import controls from './functions/controls.js';
 
-K.scene('one', () => one(controls));
-K.scene('two', () => two(controls));
+K.scene('one', () => one());
+K.scene('two', () => two());
 K.scene('win', win);
 
 K.start('one');
