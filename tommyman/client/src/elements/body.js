3a503998a96af7c3bbc8f4b4e8e9d4f843d8fde6
chiefmikey <wolfemikl@gmail.com>
2021-09-18 22:30:26 -0600

    Update body click event

--- tommyman/client/src/elements/body.js
+++ tommyman/client/src/elements/body.js
@@ -4,7 +4,18 @@ export const body = document.getElementById('body');
 
 export const bodySize = { width: body.clientWidth, height: body.clientHeight };
 
-body.addEventListener('click', (e) => {
+const beginning = new Audio('../assets/sounds/beginning.mp3');
+beginning.volume = 0.9;
+let first = true;
+
+const touchStart = (e) => {
   e.preventDefault();
+  if (first) {
+    beginning.play();
+    first = false;
+  }
   slowDown();
-});
+};
+
+body.addEventListener('click', touchStart);
+body.addEventListener('touchstart', touchStart);
