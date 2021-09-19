24ca0891d536536446c2035b9425abc8d9fa77f9
chiefmikey <wolfemikl@gmail.com>
2021-09-18 22:30:40 -0600

    Update body click event

--- tommyman/client/src/index.js
+++ tommyman/client/src/index.js
@@ -1,11 +1,1 @@
 import './controls/direction.js';
-import { body } from './elements/body.js';
-
-const beginning = new Audio('../assets/sounds/beginning.mp3');
-beginning.volume = 0.9;
-let first = true;
-if (first) {
-  body.addEventListener('click', () => beginning.play());
-  body.addEventListener('touchstart', () => beginning.play());
-  first = false;
-}
