619f1f1635a5f120fd931dcfadf7179ae8d54d02
chiefmikey <wolfemikl@gmail.com>
2021-09-18 23:12:40 -0600

    Remove console logs

--- tommyman/client/src/controls/speed.js
+++ tommyman/client/src/controls/speed.js
@@ -2,13 +2,12 @@ let speed = 1;
 
 export const speedUp = () => {
   speed += 1;
-  console.log(speed);
 };
+
 export const slowDown = () => {
   if (speed > 1) {
     speed -= 1;
   }
-  console.log(speed);
 };
 
 export const getSpeed = () => speed;
