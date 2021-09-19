b7c73f724c5d8cdc3edf507f5154f61bad5cfccb
chiefmikey <wolfemikl@gmail.com>
2021-09-18 23:08:32 -0600

    Include touch controls

--- tommyman/client/src/elements/body.js
+++ tommyman/client/src/elements/body.js
@@ -1,4 +1,4 @@
-import { slowDown } from '../controls/speed.js';
+import { speedUp, slowDown } from '../controls/speed.js';
 
 export const body = document.getElementById('body');
 
@@ -8,14 +8,41 @@ const beginning = new Audio('../assets/sounds/beginning.mp3');
 beginning.volume = 0.9;
 let first = true;
 
-const touchStart = (e) => {
-  e.preventDefault();
+const touchStart = (event) => {
   if (first) {
     beginning.play();
     first = false;
   }
+};
+
+let currentY = null;
+let touching = false;
+
+const touchMove = (event) => {
+  if (!touching && currentY < event.changedTouches[0].clientY) {
+    touching = true;
+    speedUp();
+  } else if (!touching && currentY > event.changedTouches[0].clientY) {
+    touching = true;
+    slowDown();
+  }
+  currentY = event.changedTouches[0].clientY;
+};
+
+const touchEnd = (event) => {
+  touching = false;
+};
+
+const increaseSpeed = (event) => {
+  speedUp();
+};
+
+const decreaseSpeed = (event) => {
   slowDown();
 };
 
-body.addEventListener('click', touchStart);
 body.addEventListener('touchstart', touchStart);
+body.addEventListener('touchmove', touchMove);
+body.addEventListener('touchend', touchEnd);
+body.addEventListener('click', increaseSpeed);
+body.addEventListener('contextmenu', decreaseSpeed);
--- tommyman/client/src/elements/tommyman.js
+++ tommyman/client/src/elements/tommyman.js
@@ -1,13 +1,6 @@
-import { speedUp } from '../controls/speed.js';
-
 let degrees = 0;
 
 export const tommyman = document.getElementById('tommyman');
-
-tommyman.addEventListener('click', (e) => {
-  e.preventDefault();
-  speedUp();
-});
 
 export const move = (left, bottom) => {
   tommyman.style.left = `${left}px`;
