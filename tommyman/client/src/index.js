cd8eacc3166fbd4c750db4752ff9102bd7499c5a
chiefmikey <wolfemikl@gmail.com>
2021-09-18 22:06:11 -0600

    Update touchstart

--- tommyman/client/src/index.js
+++ tommyman/client/src/index.js
@@ -1,6 +1,7 @@
 import './controls/direction.js';
+import { body } from './elements/body.js';
 
 const beginning = new Audio('../assets/sounds/beginning.mp3');
-
 beginning.volume = 0.9;
-beginning.play();
+body.addEventListener('click', () => beginning.play());
+body.addEventListener('touchstart', () => beginning.play());
