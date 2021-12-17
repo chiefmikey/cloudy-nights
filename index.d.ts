0d3403f230100d6e43dfc545e7fd1d66703309bf
Mikl Wolfe <wolfemikl@gmail.com>
2021-12-16 18:21:01 -0700

    Update types and webpack workflow to remove index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,14 +1,3 @@
-type IntervalType = (
-  callback: () => Promise<void>,
-  interval: number,
-) => NodeJS.Timer;
-
-type EventListenerType = (
-  type: string,
-  callback: () => Promise<void>,
-  options?: { passive: boolean },
-) => void;
-
 interface EventAttachType extends Document {
   attachEvent: (type: string, callback: () => void) => void;
 }
